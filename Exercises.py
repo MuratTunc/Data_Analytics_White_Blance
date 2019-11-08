cf6f00a53ab2d97e105adef3f3c2bdc957dd39fa
MuratTunc <murat.tunc8558@gmail.com>
2019-11-08 11:44:28 +0300

    Getting u,v,List ok!!!

--- Exercises.py
+++ Exercises.py
@@ -29,18 +29,6 @@ print(str_splt[3][-5:])
         Value_list[index] = 'X'          #No setting
         index +=1
 
-        str2 = '[ SET_RgbGainOffset: 	ColorTempID= 0	Factory_Comp= 0	VALUE= 124 ]'
-str_splt = str2.split(":")
-
-
-
-#print(data[0])
-
-print(str_splt[0][2:])
-
-"""
-
-
 file_path = '/home/murat/Projects/Workbench/Data_Science/Data_Analytics_White_Blance/DATA/HAT2S_T5B_056T49-D51_23.10.2019.TXT'
 
 # Open file
@@ -49,12 +37,37 @@ fileHandler = open(file_path, "r")
 # Get All lines
 listOfLines = fileHandler.readlines()
 Total_Line_Numbers = len(listOfLines)
-print('Total_Line_Numbers= ',Total_Line_Numbers)
+#print('Total_Line_Numbers= ',Total_Line_Numbers)
 
 print(listOfLines[34])
 fileHandler.close()
 
+"""
+Color_Temp = []
+Factory_Comp_list = []
+Value_list = []
+index_uv = 1
+current_line_str = '[ SET_RgbGainOffset: 	ColorTempID= 7	Factory_Comp= 0	VALUE= 124 ]'
+str_splt = current_line_str.split(":")
+str_sub = current_line_str.split("	")
 
+print(str_sub)
+print(str_sub[1][-1:])
+
+
+
+print('eeeeeeeeeeee')
+
+for i in range(1, 5):
+    Factory_Comp_list.insert(i, str_sub[1][-1:])
+    #Factory_Comp_list.append(str_sub[1][-1:])  # Get "Comp Type" value
+
+Factory_Comp_list.pop(1)
+Factory_Comp_list.insert(1, 12)
+
+
+
+print(Factory_Comp_list)
 
 
 
